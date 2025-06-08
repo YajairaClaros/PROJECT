b3f80e73918e1f5aef96fd25863fae0872ad62fc
Dennisovich503 <dennispalacios86@gmail.com>
2025-06-07 21:55:20 -0600

    Pruebas

--- Practica1.py
+++ Practica1.py
@@ -11,22 +11,41 @@ def sumar():
         resultado.config(text="Por favor, ingresa números válidos.")
 
 ventana = tk.Tk()
-ventana.title("Suma de dos números")
-ventana.geometry("700x400")
+ventana.title("Suma de dos números")#Titulo
+ventana.geometry("600x400")#Tamaño de pantalla
+ventana.minsize(500, 200)#Tamaño mínimo de pantalla
+ventana.maxsize(800, 500)#Tamaño máximo
+ventana.iconbitmap("manzana.ico")#Icono de la app
 
 
+#Ventana para aplicar configuraciones como color de fondo, cursos y bordes
+ventana.config(
+    bg="white",         # Color de fondo
+    cursor="hand2",     # Cursor del mouse
+    relief="solid",    # Borde (opcional)
+    bd=2             # Grosor del borde
+)
+
+
+ventana.config(bg="white")#Lo mismo que el anterior pero más abreviado
+
+#Entrada de texto 1
 tk.Label(ventana, text="Número 1:").pack()
 entrada1 = tk.Entry(ventana)
 entrada1.pack()
 
+#Entrada de texto 2
 tk.Label(ventana, text="Número 2:").pack()
 entrada2 = tk.Entry(ventana)
 entrada2.pack()
 
-boton_sumar = tk.Button(ventana, text="Sumar")
+#Botón para sumar
+boton_sumar = tk.Button(ventana, text="Sumar", command=sumar)
 boton_sumar.pack(padx=5, pady=5)
 
+#Etiqueta que muestra el resultado
 resultado = tk.Label(ventana, text="Resultado: ")
 resultado.pack()
 
+#Comando para evitar que la ventana se cierre al instante
 ventana.mainloop()
